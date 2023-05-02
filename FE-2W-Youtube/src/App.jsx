5db9b104304656270d24b93aa88ad50c5905c977
janghwahyun <1117jhh@gmail.com>
2023-05-03 03:00:46 +0900

    feat : search header done!

--- FE-2W-Youtube/src/App.jsx
+++ FE-2W-Youtube/src/App.jsx
@@ -1,12 +1,15 @@
-import "./App.css";
-import { Outlet } from "react-router-dom";
+import './App.css'
+import { Outlet } from 'react-router-dom'
+import SearchHeader from './components/SearchHeader'
 
 function App() {
-  return (
-    <>
-      <Outlet />
-    </>
-  );
+    return (
+        <>
+            <SearchHeader />
+            {/* children과 같은 효과 ! ✨ */}
+            <Outlet />
+        </>
+    )
 }
 
-export default App;
+export default App
--- FE-2W-Youtube/src/pages/Videos.jsx
+++ FE-2W-Youtube/src/pages/Videos.jsx
@@ -1,3 +1,7 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
 export default function Videos() {
-  return <div>VIDEOS</div>;
+    const { keyword } = useParams()
+    return <div>VIDEOS {keyword ? `${keyword}` : '응 없어'}</div>
 }
